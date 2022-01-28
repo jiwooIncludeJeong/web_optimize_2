e59d5d7a8d152af2e4500a3e2c83d6e9b6066b0e
jeongjiwoo-include <jjw89506343@gmail.com>
2022-01-28 15:10:14 +0900

    [lecture2] add preload after other components loaded and lazyWithPreload function

--- src/App.js
+++ src/App.js
@@ -1,31 +1,43 @@
-import React, {lazy, useState, Suspense} from 'react'
+import React, {lazy, useState, Suspense, useEffect} from 'react'
 import styled from 'styled-components'
 import Header from './components/Header'
 import InfoTable from './components/InfoTable'
 import SurveyChart from './components/SurveyChart'
 import Footer from './components/Footer'
 // import ImageModal from './components/ImageModal'
-const ImageModal = lazy(() => import('./components/ImageModal'));
+
+const lazyWithPreload = (importFunction) => {
+    const Component = React.lazy(importFunction)
+    Component.preload = importFunction;
+    return Component;
+}
+
+const LazyImageModal = lazyWithPreload(() => import('./components/ImageModal'));
 
 function App() {
     const [showModal, setShowModal] = useState(false)
 
     // 1. 버튼 위에 마우스가 올라갔을 때 component preload -> 모듈 파일이 너무 크다면 맞지않는 방법
-    const handleMouseEnter = async () => {
-        await import('./components/ImageModal');
-    }
+    // const handleMouseEnter = async () => {
+    //     LazyImageModal.preload();
+    // }
+
+    // 2. 모든 다른 컴포넌트의 loading이 끝났을 때 preload
+    useEffect(()=>{
+        LazyImageModal.preload();
+    },[])
 
     return (
         <div className="App">
             <Header />
             <InfoTable />
-            <ButtonModal onClick={() => { setShowModal(true) }} onMouseEnter={handleMouseEnter}>올림픽 사진 보기</ButtonModal>
+            <ButtonModal onClick={() => { setShowModal(true) }} /* onMouseEnter={handleMouseEnter}*/>올림픽 사진 보기</ButtonModal>
             {/*important*/}
             <SurveyChart />
             <Footer />
             {/*important*/}
             <Suspense fallback={null}>
-                {showModal ? <ImageModal closeModal={() => { setShowModal(false) }} /> : null}
+                {showModal ? <LazyImageModal closeModal={() => { setShowModal(false) }} /> : null}
             </Suspense>
         </div>
     )
