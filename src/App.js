ca4ce29c9f1ca464bc93a1a197747e49f531f7ad
jeongjiwoo-include <jjw89506343@gmail.com>
2022-01-28 14:57:12 +0900

    [lecture2] add bundle analyzer and lazy load image modal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react'
+import React, {lazy, useState, Suspense} from 'react'
 import styled from 'styled-components'
 import Header from './components/Header'
 import InfoTable from './components/InfoTable'
 import SurveyChart from './components/SurveyChart'
 import Footer from './components/Footer'
-import ImageModal from './components/ImageModal'
+// import ImageModal from './components/ImageModal'
+const ImageModal = lazy(() => import('./components/ImageModal'));
 
 function App() {
     const [showModal, setShowModal] = useState(false)
@@ -18,7 +19,9 @@ function App() {
             <SurveyChart />
             <Footer />
             {/*important*/}
-            {showModal ? <ImageModal closeModal={() => { setShowModal(false) }} /> : null}
+            <Suspense fallback={null}>
+                {showModal ? <ImageModal closeModal={() => { setShowModal(false) }} /> : null}
+            </Suspense>
         </div>
     )
 }
