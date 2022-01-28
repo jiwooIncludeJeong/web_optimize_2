f4bc12b05d0fa376ecdea7db4f046c0d12ce2f4f
jeongjiwoo-include <jjw89506343@gmail.com>
2022-01-28 15:03:54 +0900

    [lecture2] add preload when mouse enter the button

--- src/App.js
+++ src/App.js
@@ -10,11 +10,16 @@ const ImageModal = lazy(() => import('./components/ImageModal'));
 function App() {
     const [showModal, setShowModal] = useState(false)
 
+    // 1. 버튼 위에 마우스가 올라갔을 때 component preload -> 모듈 파일이 너무 크다면 맞지않는 방법
+    const handleMouseEnter = async () => {
+        await import('./components/ImageModal');
+    }
+
     return (
         <div className="App">
             <Header />
             <InfoTable />
-            <ButtonModal onClick={() => { setShowModal(true) }}>올림픽 사진 보기</ButtonModal>
+            <ButtonModal onClick={() => { setShowModal(true) }} onMouseEnter={handleMouseEnter}>올림픽 사진 보기</ButtonModal>
             {/*important*/}
             <SurveyChart />
             <Footer />
